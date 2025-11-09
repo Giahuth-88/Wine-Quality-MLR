847eeda0c6da368557468e18d6e91730d374a5b5
Gia Hu <jia.hu.th@dartmouth.edu>
2025-11-08 21:30:41 -0500

    Fix image path for Correlation Heatmap

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ st.divider()
 st.header("📈 Data Visualization")
 col1, col2 = st.columns(2)
 with col1:
-    st.image("Assets/Correlation Heatmap .png", caption="Correlation Heatmap")
+    st.image("Assets/Correlation Heatmap.png", caption="Correlation Heatmap")
 with col2:
     st.image("Assets/Pairwise Relationships.png", caption="Pairwise Relationships")
 
